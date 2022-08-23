d87d07ed2ae9b3713478264e206aa183dae24156
Alena-007 <alena.tv@ukr.net>
2022-08-24 01:41:32 +0300

    update files2

--- src/js/getTrendingMovies.js
+++ src/js/getTrendingMovies.js
@@ -1,14 +1,9 @@
-import axios from 'axios';
 export { getTrendingMovies };
 
 const API_KEY = '8f869b0e17fd67e41e1d615a0551fc3f';
 
-async function getTrendingMovies() {
-  try {
-    const URL = `https://api.themoviedb.org/3/trending/movie/day?api_key=${API_KEY}`;
-    const response = await axios.get(URL);
-    return response.data;
-  } catch (error) {
-    console.log(error);
-  }
+function getTrendingMovies(page = 1) {
+  return fetch(
+    `https://api.themoviedb.org/3/trending/movie/day?api_key=${API_KEY}&page=${page}`
+  ).then(respons => respons.json());
 }
