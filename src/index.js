5d3dcb536bb5124d0188ed4ce6581194b37d7e88
Alena Busova <96522313+al-busova@users.noreply.github.com>
2022-08-28 00:36:42 +0300

    move code pagin

--- src/index.js
+++ src/index.js
@@ -2,15 +2,13 @@ import { getPopularMovies } from './js/getFetch';
 import { getSearchMovies } from './js/getFetch';
 import { getMovieById } from './js/getFetch';
 import { showPopularMovieGallery } from './js/showPopularMovieGallery';
-import { movieGallery } from './js/renderMovieGallery';
+import { getPagination } from './js/pagination';
 // import { onOpenModal } from './js/modal';
 // import { onCloseModal } from './js/modal';
 import modal from './js/modal'
 
 import axios from 'axios';
 
-import Pagination from 'tui-pagination';
-// import 'tui-pagination/dist/tui-pagination.css';
 
 const popularBtn = document.querySelector('#popular');
 const searchBtn = document.querySelector('#search-btn');
@@ -21,7 +19,7 @@ searchBtn.addEventListener('click', onSearch);
 movieByIdBtn.addEventListener('click', onMovieById);
 
 showPopularMovieGallery(1);
-
+getPagination();
 async function onPopular() {
   const data = await getPopularMovies(1);
   console.log(data);
@@ -37,24 +35,3 @@ async function onMovieById() {
   return data;
   console.log(data);
 }
-function resetPopular() {
-  movieGallery.innerHTML = '';
-}
-const pagination = new Pagination('tui-pagination-container', {
-  totalItems: 0,
-  itemsPerPage: 20,
-  visiblePages: 5,
-  page: 1,
-  centerAlign: true,
-});
-
-pagination.on('afterMove', event => {
-  resetPopular();
-  showPopularMovieGallery(event.page);
-});
-
-getPopularMovies(1)
-  .then(data => {
-    pagination.reset(data.total_results);
-  })
-  .catch(error => console.log(error));
