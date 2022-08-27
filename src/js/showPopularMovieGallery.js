ba7d82f39802ef4fe67e165e2ceb699c4183dd3d
Oleh Okarynskyi <okarynskyy@gmail.com>
2022-08-27 13:13:03 +0300

    add loader.js

--- src/js/showPopularMovieGallery.js
+++ src/js/showPopularMovieGallery.js
@@ -1,14 +1,17 @@
 import { renderMovieGallery } from './renderMovieGallery';
 import { getPopularMovies } from './getFetch';
+import { showLoader, hideLoader } from './loader';
 
 export function showPopularMovieGallery() {
   let page = 1;
+  showLoader()
 
   getPopularMovies(page)
     .then(data => {
       const filmsArray = data.results;
       console.log(filmsArray);
       renderMovieGallery(filmsArray);
+      hideLoader()
     })
     .catch(error => console.log(error));
 }
