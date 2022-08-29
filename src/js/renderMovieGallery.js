232007c7ca75b386b26e21e61c5cd6c46b9e89a4
Andrey Avdeev <a3avdeev@gmail.com>
2022-08-30 01:27:43 +0300

    genres added

--- src/js/renderMovieGallery.js
+++ src/js/renderMovieGallery.js
@@ -1,16 +1,8 @@
 // import { movieGallery } from '../index';
 
-import { getGenres } from './getFetch';
+// import { getGenres } from './getFetch';
 
 export const movieGallery = document.querySelector('.gallery');
-
-// getGenres().then(data => {
-//   const genreName = data.genres.map(genreName => genreName.name);
-//   const genreId = data.genres.map(genreName => genreName.id);
-//   console.log(genreName);
-//   console.log(genreId);
-//   console.log(getGenres());
-// });
 
 export const renderMovieGallery = filmsArray => {
   const genresList = {
@@ -37,24 +29,15 @@ export const renderMovieGallery = filmsArray => {
 
   const result = filmsArray
     .map(({ poster_path, original_title, genre_ids, release_date, id }) => {
-      const idsKeys = Object.keys(genresList);
-      const idsValues = Object.values(genresList);
-      // const genres = genre_ids.map(i => i);
-      // console.log(genres);
-      const idKey = idsKeys.map(Number);
-      const idVal = idsValues.map(val => val);
-      // console.log(idKey);
-      // console.log(idVal);
+      const idKey = Object.keys(genresList).map(Number);
+      const idVal = Object.entries(genresList).map(val => val);
+
       const genreName = [];
-      for (let value of idVal) {
-        // console.log(value);
-      }
       for (let genre of genre_ids) {
-        // console.log(genre);
         for (let key of idKey) {
-          // console.log(key);
           if (key === genre) {
-            genreName.push(idVal[5]);
+            let num = idKey.indexOf(key);
+            genreName.push(idVal[num][1]);
           }
         }
       }
@@ -67,7 +50,10 @@ export const renderMovieGallery = filmsArray => {
               ${original_title}
             </p>
             <p class="gallery__item-text">
-              ${genreName} | ${release_date.substr(0, 4)}
+              ${genreName
+                .slice(0, 3)
+                .map(a => a)
+                .join(', ')} | ${release_date.substr(0, 4)}
             </p>
           </div>
         </a>
@@ -77,25 +63,3 @@ export const renderMovieGallery = filmsArray => {
 
   movieGallery.insertAdjacentHTML('beforeend', result);
 };
-
-// const genresList = {
-//   0: { id: 28, name: 'Action' },
-//   1: { id: 12, name: 'Adventure' },
-//   2: { id: 16, name: 'Animation' },
-//   3: { id: 35, name: 'Comedy' },
-//   4: { id: 80, name: 'Crime' },
-//   5: { id: 99, name: 'Documentary' },
-//   6: { id: 18, name: 'Drama' },
-//   7: { id: 10751, name: 'Family' },
-//   8: { id: 14, name: 'Fantasy' },
-//   9: { id: 36, name: 'History' },
-//   10: { id: 27, name: 'Horror' },
-//   11: { id: 10402, name: 'Music' },
-//   12: { id: 9648, name: 'Mystery' },
-//   13: { id: 10749, name: 'Romance' },
-//   14: { id: 878, name: 'Science Fiction' },
-//   15: { id: 10770, name: 'TV Movie' },
-//   16: { id: 53, name: 'Thriller' },
-//   17: { id: 10752, name: 'War' },
-//   18: { id: 37, name: 'Western' },
-// };
